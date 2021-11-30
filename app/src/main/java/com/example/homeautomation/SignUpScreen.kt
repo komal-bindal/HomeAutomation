b841d68e6a8697688d60f64650259ae217595054
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-11-30 20:45:44 +0530

    modifications in signup screen

--- app/src/main/java/com/example/homeautomation/SignUpScreen.kt
+++ app/src/main/java/com/example/homeautomation/SignUpScreen.kt
@@ -1,11 +1,19 @@
 package com.example.homeautomation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUpScreen : AppCompatActivity() {
+    private lateinit var signin: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_screen)
+        signin = findViewById(R.id.signinText)
+        signin.setOnClickListener {
+            startActivity(Intent(this, SigninScreen::class.java))
+            finish()
+        }
     }
 }
