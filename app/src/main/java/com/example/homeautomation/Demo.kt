65fe52103aa2e3661523705c4c52a8481949043b
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-11-30 20:00:32 +0530

    design signup screen for home automation

---
+++ app/src/main/java/com/example/homeautomation/Demo.kt
@@ -0,0 +1,11 @@
+package com.example.homeautomation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Demo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
--- app/src/main/java/com/example/homeautomation/MainActivity.kt
+++ app/src/main/java/com/example/homeautomation/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.homeautomation
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.imageView2)
+        val angle = 45;
+        val pivotX = 20;
+        startActivity(Intent(this, Demo::class.java))
     }
 }
