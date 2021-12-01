6853bfec7df6e9d22e525bbf8d02d4cd96480d79
Kritika sharma <kritika.sharma_cs19@gla.ac.in>
2021-12-01 13:13:52 +0530

    created the otp login screen for the user to login through otp

---
+++ app/src/main/java/com/example/homeautomation/OtpLogin.kt
@@ -0,0 +1,87 @@
+package com.example.homeautomation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+class OtpLogin : AppCompatActivity() {
+    private lateinit var phoneNumberEditText: EditText
+    private lateinit var getOtpButton: Button
+    private lateinit var verificationOTP: String
+    private val PHONE_NUMBER_ERROR = "phone number should have length 10"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_otp_login)
+
+        phoneNumberEditText = findViewById(R.id.phoneEditText)
+        getOtpButton = findViewById(R.id.signinPhoneNumberButton)
+
+        phoneNumberEditText.requestFocus()
+
+        val intent = intent
+        val user = intent.getStringExtra("User").toString()
+
+        getOtpButton.setOnClickListener(
+            object : View.OnClickListener {
+                override fun onClick(v: View) {
+                    if (phoneNumberEditText.text.toString().trim().isEmpty()) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Please enter phone number",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        return
+                    }
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        "+91" + phoneNumberEditText.text.toString(),
+                        60,
+                        TimeUnit.SECONDS,
+                        this@OtpLogin,
+                        object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            override fun onCodeSent(
+                                verificationId: String,
+                                forceResendingToken: PhoneAuthProvider.ForceResendingToken
+                            ) {
+                                super.onCodeSent(verificationId, forceResendingToken)
+                                verificationOTP = verificationId
+                                val phoneNumber = "+91 " + phoneNumberEditText.text.toString()
+                                val intent =
+                                    Intent(applicationContext,OtpLogin::class.java)
+                                intent.putExtra("PhoneNumber", phoneNumber.toString())
+                                intent.putExtra("VerificationOTP", verificationOTP.toString())
+                                intent.putExtra("User", user)
+                                startActivity(intent)
+                                finish()
+                            }
+
+                            override fun onVerificationCompleted(phoneAuthCredential: PhoneAuthCredential) {
+
+                            }
+
+                            override fun onVerificationFailed(e: FirebaseException) {
+                                Toast.makeText(applicationContext, e.message, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    )
+                }
+            }
+        )
+
+
+    }
+}
